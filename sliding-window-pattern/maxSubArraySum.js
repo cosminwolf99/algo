e04a7a7ed654ff361d705155728ddcb555f81ddb
cosminwolf99 <cosmin.lup16@gmail.com>
2025-03-16 16:35:50 +0200

    add more recursive solutions

--- sliding-window-pattern/maxSubArraySum.js
+++ sliding-window-pattern/maxSubArraySum.js
@@ -1,4 +1,5 @@
 function maxSubArraySum(arr, num) {
+<<<<<<< HEAD
    let maxSum = 0;
    let tempSum = 0;
 
@@ -12,10 +13,23 @@ function maxSubArraySum(arr, num) {
          tempSum = tempSum - arr[j - num] + arr[j];
          maxSum = Math.max(maxSum, tempSum)
    }
+=======
+  let maxSum = 0;
+  let tempSum = 0;
 
-   return maxSum
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+>>>>>>> 258d798 (add more recursive solutions)
+
+  return maxSum;
 }
 
-
 // input: [2,6,9,2,1,8,5,6,3], 3
-console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 3))
+console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
