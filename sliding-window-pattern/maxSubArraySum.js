801ec03f354050800c53a5ff67f8297cb1fe23da
cosminlup16 <cosmin.lup@8x8.com>
2025-03-17 15:00:53 +0200

    recap

--- sliding-window-pattern/maxSubArraySum.js
+++ sliding-window-pattern/maxSubArraySum.js
@@ -1,15 +1,16 @@
 function maxSubArraySum(arr, num) {
-    let maxSum = 0;
-    let tempSum = 0;
+   let maxSum = 0;
+   let tempSum = 0;
 
-    for (let i = 0; i < num; i++) {
-        maxSum += arr[i];
-    }
-    tempSum = maxSum;
+   for (let i = 0; i < num; i++) {
+       maxSum += arr[i]
+   }
 
-   for (let i = num; i < arr.length; i++) {
-        tempSum = tempSum - arr[i - num] + arr[i];
-        maxSum = Math.max(maxSum, tempSum);
+   tempSum = maxSum;
+
+   for (let j = num; j < arr.length; j++) {
+         tempSum = tempSum - arr[j - num] + arr[j];
+         maxSum = Math.max(maxSum, tempSum)
    }
 
    return maxSum
